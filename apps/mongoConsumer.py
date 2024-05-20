b4d880c29bb49312938ddbd8a32813d0f5c527aa
Edwin Leandro Montoya Osorio <emontoya@sdi.es>
2024-05-20 23:50:27 +0200

    creando volumen a los jar

--- apps/mongoConsumer.py
+++ apps/mongoConsumer.py
@@ -2,7 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, StructField
 
-
 def consumir_datos_de_kafka():
     aws_access_key_id = 'test'
     aws_secret_access_key = 'test'
@@ -14,15 +13,11 @@ def consumir_datos_de_kafka():
         .config("spark.hadoop.fs.s3a.endpoint", "http://spark-localstack-1:4566") \
         .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
         .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
-        .config("spark.jars.packages",
-                "org.apache.spark:spark-hadoop-cloud_2.13:3.5.1,software.amazon.awssdk:s3:2.25.11,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
+        .config("spark.jars", "file:///opt/spark/jars/spark-sql-kafka-0-10_2.12-3.2.0.jar,file:///opt/spark/jars/spark-token-provider-kafka-0-10_2.12-3.2.0.jar") \
         .config("spark.hadoop.fs.s3a.path.style.access", "true") \
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-        .config("spark.driver.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
-        .config("spark.executor.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
         .master("spark://spark-master:7077") \
         .getOrCreate()
-    # Crear una sesión de Spark
 
     # Definir el esquema de los datos
     esquema = StructType([
@@ -32,7 +27,7 @@ def consumir_datos_de_kafka():
         StructField("preferencias_dieteticas", StringType(), True)
     ])
 
-    # Leer los datos de Kafka
+
     df_kafka = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "kafka:9092") \
