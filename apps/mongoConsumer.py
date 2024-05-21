428e071ec58153788e74e601ce5f625699fd089f
Edwin Leandro Montoya Osorio <emontoya@sdi.es>
2024-05-21 08:47:45 +0200

    cambiando esquema

--- apps/mongoConsumer.py
+++ apps/mongoConsumer.py
@@ -3,43 +3,38 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, StructField
 
 def consumir_datos_de_kafka():
-    aws_access_key_id = 'test'
-    aws_secret_access_key = 'test'
+
 
     spark = SparkSession.builder \
-        .appName("Streaming from Kafka") \
-        .config("spark.streaming.stopGracefullyOnShutdown", True) \
-        .config("spark.sql.shuffle.partitions", 4) \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://spark-localstack-1:4566") \
-        .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
-        .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
-        .config("spark.jars", "file:///opt/spark/jars/spark-sql-kafka-0-10_2.12-3.2.0.jar,file:///opt/spark/jars/spark-token-provider-kafka-0-10_2.12-3.2.0.jar") \
-        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .appName("mongo consumer") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.sql.shuffle.partitions", "2") \
+        .config("spark.jars", "opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar,opt/spark-apps/drivers/spark-token-provider-kafka-0-10_2.12-3.2.0.jar") \
+        .config("spark.driver.extraClassPath", "/opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar") \
+        .config("spark.executor.extraClassPath", "/opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar") \
         .master("spark://spark-master:7077") \
         .getOrCreate()
 
     # Definir el esquema de los datos
     esquema = StructType([
-        StructField("ID", StringType(), True),
+        StructField("id_cliente", StringType(), True),
         StructField("nombre", StringType(), True),
         StructField("direccion", StringType(), True),
-        StructField("preferencias_dieteticas", StringType(), True)
+        StructField("preferencias_alimenticias", StringType(), True)
     ])
 
-
+    #Leer los datos de Kafka
     df_kafka = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("subscribe", "clientes_stream") \
-        .option("startingOffsets", "earliest") \
         .load()
 
     # Convertir los datos del formato JSON
     df_valores = df_kafka.selectExpr("CAST(value AS STRING)")
     df_datos = df_valores.select(from_json(col("value"), esquema).alias("data")).select("data.*")
 
-    # Procesar los datos (aquí puedes agregar transformaciones adicionales)
+    # Procesar los datos
     query = df_datos.writeStream \
         .format("console") \
         .outputMode("append") \
--- apps/streamingKafka.py
+++ apps/streamingKafka.py
@@ -1,65 +1,45 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json
-from pyspark.sql.types import StructType, StringType, IntegerType, DoubleType
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StringType, StructField
 
-aws_access_key_id='test'
-aws_secret_access_key='test'
+def consumir_datos_de_kafka():
+    spark = SparkSession.builder \
+        .appName("Streaming from Kafka") \
+        .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+        .config("spark.sql.shuffle.partitions", "4") \
+        .config("spark.jars", "file:///opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar,file:///opt/spark-apps/drivers/spark-token-provider-kafka-0-10_2.12-3.2.0.jar") \
+        .config("spark.driver.extraClassPath", "/opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar") \
+        .config("spark.executor.extraClassPath", "/opt/spark-apps/drivers/spark-sql-kafka-0-10_2.12-3.2.0.jar") \
+        .master("spark://spark-master:7077") \
+        .getOrCreate()
 
-spark = SparkSession.builder \
-    .appName("Streaming from Kafka") \
-    .config("spark.streaming.stopGracefullyOnShutdown", True) \
-    .config("spark.sql.shuffle.partitions", 4) \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://spark-localstack-1:4566") \
-    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
-    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
-    .config("spark.jars.packages","org.apache.spark:spark-hadoop-cloud_2.13:3.5.1,software.amazon.awssdk:s3:2.25.11,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
-    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .config("spark.driver.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
-    .config("spark.executor.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
-    .master("spark://spark-master:7077") \
-    .getOrCreate()
-    
-    
-       
-df =spark  \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "kafka:9093") \
-  .option("subscribe", "sales_stream") \
-  .load()
-  
+    # Definir el esquema de los datos
+    esquema = StructType([
+        StructField("ID", StringType(), True),
+        StructField("nombre", StringType(), True),
+        StructField("direccion", StringType(), True),
+        StructField("preferencias_dieteticas", StringType(), True)
+    ])
 
-schema = StructType() \
-    .add("timestamp", IntegerType()) \
-    .add("store_id", IntegerType()) \
-    .add("product_id", StringType()) \
-    .add("quantity_sold", IntegerType()) \
-    .add("revenue", DoubleType())
+    # Leer los datos de Kafka
+    df_kafka = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
+        .option("subscribe", "clientes_stream") \
+        .option("startingOffsets", "earliest") \
+        .load()
 
-# Convert value column to JSON and apply schema
-df = df.selectExpr("CAST(value AS STRING)") \
-    .select(from_json("value", schema).alias("data")) \
-    .select("data.*")
+    # Convertir los datos del formato JSON
+    df_valores = df_kafka.selectExpr("CAST(value AS STRING)")
+    df_datos = df_valores.select(from_json(col("value"), esquema).alias("data")).select("data.*")
 
-# Print schema of DataFrame for debugging
-df.printSchema()
+    # Procesar los datos (aquí puedes agregar transformaciones adicionales)
+    query = df_datos.writeStream \
+        .format("console") \
+        .outputMode("append") \
+        .start()
 
-# Start the computation
-query = df \
-    .writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+    query.awaitTermination()
 
-query = df \
-    .writeStream \
-    .outputMode("append") \
-    .format("csv") \
-    .option("path", "s3a://new-sample-bucket/sales") \
-    .option("checkpointLocation", "s3a://new-sample-bucket/checkopoint")\
-    .option("header", "true")\
-    .start()
-
-# Wait for the termination of the query
-query.awaitTermination()
+if __name__ == "__main__":
+    consumir_datos_de_kafka()
